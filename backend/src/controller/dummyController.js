c0585585611e0f583cb95d5045bf271087778ac9
revzik <b.piekarz1997@gmail.com>
2021-07-17 22:40:01 +0200

    Add dummy data generator

---
+++ backend/src/controller/dummyController.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+
+function strip(number) {
+  return parseFloat(parseFloat(number).toPrecision(7));
+}
+
+function randomize(amplitude) {
+  let min = 0.9 * amplitude;
+  let max = 1.1 * amplitude;
+  return Math.random() * (max - min) + min;
+}
+
+function getOrderSpectrum(max_order, dOrder, orderAmplitudes, noiseAmplitude) {
+  let spec = [];
+  let nextInt = 0;
+  for (let i = 0; i <= max_order; i += dOrder) {
+    let rounded = strip(i);
+    if (rounded < nextInt) {
+      spec.push(randomize(noiseAmplitude));
+    } else {
+      spec.push(randomize(orderAmplitudes[nextInt]));
+      nextInt++;
+    }
+  }
+
+  return spec;
+}
+
+function getDummyData() {
+  let dOrder = 0.2;
+  let orderAmpl = [0.3, 1.8, 0.4, 1, 0.3, 0.5, 0.3, 0.2, 0.8, 0.2, 0.1];
+  let orderSpec = getOrderSpectrum(
+    orderAmpl.length - 1,
+    dOrder,
+    orderAmpl,
+    0.05
+  );
+  let x = 0;
+  let spec = [];
+  orderSpec.forEach((y) => {
+    spec.push({ x: x, y: y });
+    x = strip(x + dOrder);
+  });
+
+  return {
+    freq: randomize(40),
+    rms: randomize(1.8),
+    kurt: randomize(3),
+    peak: randomize(0.7),
+    spec: spec,
+  };
+}
+
+router.get("/", (req, res) => {
+  res.json(getDummyData());
+});
+
+module.exports = router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,19 @@
 const express = require("express");
-
 const app = express();
 
 const port = process.env.PORT || 4200;
+const dummy = process.argv.slice(2).includes("--dummy");
 
 app.get("/*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-function getData() {
-  const orderSpec = {
-    o0: 0,
-    do: 0.5,
-    spec: [0, 0.1, 2.5, 0, 0.3, 0.1, 0.6, 0.2, 1.4, 0.1, 0.5, 0, 0.1],
-  };
-
-  let spec = [];
-  let x = orderSpec.o0;
-  orderSpec.spec.forEach((y) => {
-    spec.push({ x: x, y: y });
-    x += orderSpec.do;
-  });
-
-  return {
-    freq: 41.7,
-    rms: 1.5,
-    kurt: 3.05,
-    peak: 0.707,
-    spec: spec,
-  };
+let dataController = null;
+if (dummy) {
+  dataController = require(__dirname + '/controller/dummyController');
 }
 
-app.get("/data", (req, res) => {
-  res.json(getData());
-});
+app.use('/data', dataController);
 
 app.listen(port);
